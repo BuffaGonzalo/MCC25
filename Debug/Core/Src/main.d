d5508702e109b497d4c88a14a37125eb0fd75f46
BuffaGonzalo <gonzalomartinbuffa@gmail.com>
2025-06-02 14:54:37 -0300

    Adds img.h file

--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -43,8 +43,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
  ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
  ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h \
- ../Core/Inc/util.h ../Core/Inc/ssd1306.h ../Core/Inc/ssd1306_conf.h \
- ../Core/Inc/fonts.h ../Core/Inc/ssd1306.h ../Core/Inc/unerPrtcl.h
+ ../Core/Inc/util.h ../Core/Inc/img.h ../Core/Inc/ssd1306.h \
+ ../Core/Inc/ssd1306_conf.h ../Core/Inc/fonts.h ../Core/Inc/ssd1306.h \
+ ../Core/Inc/unerPrtcl.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -92,6 +93,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
 ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
 ../Core/Inc/util.h:
+../Core/Inc/img.h:
 ../Core/Inc/ssd1306.h:
 ../Core/Inc/ssd1306_conf.h:
 ../Core/Inc/fonts.h:
